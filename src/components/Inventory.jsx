646931d85780ec60b57497866aad34e4af775fd6
zekoff <zekoff@gmail.com>
2022-04-14 23:48:26 +0000

    Make inventory into list

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -1,5 +1,12 @@
+import { List, ListItem, ListItemText } from "@mui/material";
+
 function Inventory(props) {
-    return <p>{props.inventory}</p>;
+    const inventory_list = props.inventory.map(item =>
+        <ListItem>
+            <ListItemText>{item}</ListItemText>
+        </ListItem>
+    );
+    return <List>{inventory_list}</List>;
 }
 
 export default Inventory;
